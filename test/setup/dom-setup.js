ec1c19788bd1b71eaef158d838da63e1df6bb12c
Dmitry O <auuine@gmail.com>
2026-03-16 11:19:22 +0200

    Update migration to TS

--- test/setup/dom-setup.js
+++
@@ -1,7 +0,0 @@
-['qunit', 'qunit-fixture'].forEach(function(id) {
-  if (!document.getElementById(id)) {
-    var el = document.createElement('div');
-    el.id = id;
-    document.body.appendChild(el);
-  }
-});
